bc363dde7339e0a935283c5b08c04f0b5441909a
Harrison <harrisonlmitchell@gmail.com>
2015-08-13 22:58:01 -0400

    added more project files

---
+++ README.md
@@ -0,0 +1,3 @@
+To launch app:
+
+* run command: $ python entertainment_center.py
---
+++ media.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__ (self, title, storyline, release_date, lead_actor, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        self.storyline = storyline
+        self.release_date = release_date
+        self.lead_actor = lead_actor
